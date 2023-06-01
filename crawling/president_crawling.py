e4624e594ee188ea2cb01409cc4915097e821bc8
takeda <takeda@yoshidaruinoMacBook-Pro.local>
2023-06-01 16:12:43 +0900

    president

---
+++ crawling/president_crawling.py
@@ -0,0 +1,56 @@
+import httpx
+import json
+from bs4 import BeautifulSoup as bs
+from time import sleep
+
+def get_page_num(soup):
+    pagination = soup.find_all('a', class_='article-above-pagination__item')
+    return len(pagination)
+
+def get_article(url, texts, firts_page=False):
+    with httpx.Client() as client:
+        response = client.get(url)
+    soup = bs(response.text, 'html.parser')
+    if firts_page:
+        texts.append(soup.find('h1').text)
+        page_num = get_page_num(soup)
+    else:
+        page_num = 0
+    conts = soup.find_all('p')
+    for cont in conts[:-1]: # 最後は「© 2008-2023 PRESIDENT Inc. すべての画像・データについて無断転用・無断転載を禁じます。」
+        texts.append(cont.text)
+    
+    return page_num
+
+if __name__ == '__main__':
+    for i in range(1, 70165):
+        url_base = f'https://president.jp/articles/-/{i}'
+        texts = []
+        try:
+            page_num = get_article(url_base, texts, firts_page=True)
+        except:
+            continue
+
+        for n_page in range(2, page_num):
+            try:
+                url = url_base + f'?page={n_page}'
+                _ = get_article(url, texts)
+            except: # 念の為例外処理したが、発生せず
+                print(f'{url} でエラー')
+                break
+        
+        output = {'text': ''.join(texts), 'url': url_base}
+        with open('president.jsonl', 'a', encoding='utf-8') as file:
+            json.dump(output, file, ensure_ascii=False)
+            file.write('\n')
+        sleep(0.1)
+
+
+
+'''
+print(soup.prettify())
+print(soup.find('h1')) # タイトル
+print(soup.find('h4')) # 小見出し。今回は省略
+print(soup.find('div', class_='article-lead')) # リード文
+print(soup.find_all('p')) # 本文
+'''
